3c9bf84faa023ecfeb88120e327ad58ce037218c
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-03-14 12:24:50 +0100

    continous control example

---
+++ continuous/walker_example.py
@@ -0,0 +1,19 @@
+import gymnasium as gym
+
+from continuous.controller import RandomContinuousController
+
+if __name__ == '__main__':
+    # TODO make render work
+    env = gym.make("Walker2d-v4")
+    controller = RandomContinuousController(env.action_space)
+    episode_length = 1000
+
+    observation, info = env.reset(seed=0)
+    for _ in range(episode_length):
+        action = controller.control(observation)
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        if terminated or truncated:
+            observation, info = env.reset()
+
+    env.close()
