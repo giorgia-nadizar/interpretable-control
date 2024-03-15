ae16b29a06d8ad9ab23fbdff2608561fc5eb729b
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-03-15 13:51:55 +0100

    new obs type

--- discrete/env_2048/env2048.py
+++ discrete/env_2048/env2048.py
@@ -1,7 +1,6 @@
 import gymnasium as gym
 import numpy as np
-from gymnasium.spaces import Discrete
-from gymnasium.spaces.multi_discrete import MultiDiscrete
+from gymnasium import spaces
 from typing import Optional
 import random
 from typing import List, Tuple, Dict, Any
@@ -21,8 +20,8 @@ class Env2048(gym.Env):
         if render_mode is not None and render_mode != 'terminal':
             raise AttributeError(f'Render_mode is {render_mode}, but it must be either terminal or None (if disabled).')
 
-        self.action_space: Discrete = Discrete(4, seed=seed)
-        self.observation_space: MultiDiscrete = MultiDiscrete([11] * 16, seed=seed)
+        self.action_space: spaces.Discrete = spaces.Discrete(4, seed=seed)
+        self.observation_space: spaces.Box = spaces.Box(0, 2048, shape=(4, 4), dtype=int, seed=seed)
 
         self.seed: Optional[int] = seed
         self.render_mode: Optional[str] = render_mode
